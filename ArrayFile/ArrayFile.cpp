615a88c40b456d491b0eff26068cf37c2c23b0ce
Misha Anisimov <anisimov.mykhailo@chnu.edu.ua>
2024-02-23 13:29:20 +0200

    Task 2 Array

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -2,9 +2,25 @@
 #include <vector>
 
 void array();
+int array2(int A[], int N, int T);
 
 int main() {
-    array();
+
+    //array();
+
+    const int N = 10; 
+    int A[N] = {10, 3, 5, 7, 9, 6, 4, 2, 8, 1}; 
+    int T = 6;
+
+    int result = array2(A, N, T);
+
+    if (result != -1) {
+        std::cout << "The number of the last maximum positive element greater than " << T << ": " << result << std::endl;
+    } else {
+        std::cout << "There are no positive elements greater than " << T << std::endl;
+    }
+
+    system("pause");
     return 0;
 }
 
@@ -31,6 +47,18 @@ void array() {
         std::cout << B[i] << " ";
     }
     std::cout << "\n";
+}
 
-    system("pause");
+int array2(int A[], int N, int T) {
+    int lastMaxIndex = -1; 
+    int maxElement = INT_MIN; 
+
+    for (int i = 0; i < N; ++i) {
+        if (A[i] > T && A[i] > maxElement) {
+            maxElement = A[i];
+            lastMaxIndex = i;
+        }
+    }
+
+    return lastMaxIndex;
 }
